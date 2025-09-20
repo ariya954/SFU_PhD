f143506961bbf4f06a16a978b08cebafe36a9d64
Mohammad Taghizadeh <ariyataghizadeh@gmail.com>
2025-09-20 11:29:07 -0700

    Add Robot Collision Detection Project

---
+++ Robot_Collision_Detection/gem5-SALAM/collision_detection_with_MLP/hw/hw_defines.h
@@ -0,0 +1,11 @@
+//Standard Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include "../defines.h"
+#include "../collision_detection_clstr_hw_defines.h"
+
+#define DEV_INIT	0x01
+#define DEV_INTR	0x04
